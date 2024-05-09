f75e9687906bfde9fc5973d2166709860680918d
Riyaz Patnam <105287336+riyazpatnam@users.noreply.github.com>
2024-05-09 13:06:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example01"
+resource "azurerm_resource_group" "example02" {
+  name     = "example02"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "example03" {
+  name     = "example03"
   location = "West Europe"
 }
